ef79322c3aa830810bfa254b982becccb8a1d6ab
NIKODY\anton <antonnik00@mail.ru>
2024-02-24 15:54:10 +0300

    My first commit

---
+++ create_user_test.py
@@ -0,0 +1,136 @@
+import sender_stand_request
+import data
+
+# эта функция меняет значения в параметре firstName
+def get_user_body(first_name):
+    # копирование словаря с телом запроса из файла data, чтобы не потерять данные в исходном словаре
+    current_body = data.user_body.copy()
+    # изменение значения в поле firstName
+    current_body["firstName"] = first_name
+    # возвращается новый словарь с нужным значением firstName
+    return current_body
+
+def test_create_user_2_letter_in_first_name_get_success_response():
+    user_body = get_user_body("Аа")
+    user_response = sender_stand_request.post_new_user(user_body)
+    users_table_response = sender_stand_request.get_users_table()
+
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+
+    assert user_response.status_code == 201
+    assert user_response.json()["authToken"] != ""
+    assert users_table_response.text.count(str_user) == 1
+
+# Функция для позитивной проверки
+def positive_assert(first_name):
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body(first_name)
+    # В переменную user_response сохраняется результат запроса на создание пользователя:
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201
+    # Проверяется, что в ответе есть поле authToken и оно не пустое
+    assert user_response.json()["authToken"] != ""
+
+    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
+    users_table_response = sender_stand_request.get_users_table()
+
+    # Строка, которая должна быть в ответе
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+
+    # Проверка, что такой пользователь есть и он единственный
+    assert users_table_response.text.count(str_user) == 1
+
+# Тест 1. Успешное создание пользователя
+# Параметр fisrtName состоит из 2 символов
+def test_create_user_2_letter_in_first_name_get_success_response():
+    positive_assert("Ааааааааааааааа")
+
+def negative_assert_symbol(first_name):
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body(first_name)
+    # В переменную user_response сохраняется результат запроса на создание пользователя:
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 400
+    assert user_response.status_code == 400
+
+    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
+    users_table_response = sender_stand_request.get_users_table()
+
+    assert user_response.json()["code"] == 400
+    assert user_response.json()["message"] == "Имя пользователя введено некорректно. Имя может содержать только русские или латинские буквы, длина должна быть не менее 2 и не более 15 символов"
+
+def test_negative():
+    negative_assert_symbol("А")
+
+def test_create_user_1_letter_in_first_name_get_error_response(first_name):
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body(first_name)
+    # В переменную user_response сохраняется результат запроса на создание пользователя:
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 400
+    assert user_response.status_code == 400
+
+    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
+    users_table_response = sender_stand_request.get_users_table()
+
+    assert user_response.json()["code"] == 400
+    assert user_response.json()[
+               "message"] == "Имя пользователя введено некорректно. Имя может содержать только русские или латинские буквы, длина должна быть не менее 2 и не более 15 символов"
+
+def test_third():
+    negative_assert_symbol("Аааааааааааааааа")
+
+def test_create_user_16_letter_in_first_name_get_error_response():
+    positive_assert("QWErty")
+
+def test_create_user_english_letter_in_first_name_get_success_response():
+    positive_assert("Мария")
+
+def negative_assert_no_first_name(user_body):
+    # В переменную user_response сохраняется результат запроса на создание пользователя:
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 400
+    assert response.status_code == 400
+
+    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
+    users_table_response = sender_stand_request.get_users_table()
+
+    assert response.json()["code"] == 400
+    assert response.json()[
+               "message"] == "Не все необходимые параметры были переданы"
+
+def test_2():
+    negative_assert_no_first_name()
+
+# Тест 10. Ошибка
+# В запросе нет параметра firstName
+def test_create_user_no_first_name_get_error_response():
+    # Копируется словарь с телом запроса из файла data в переменную user_body
+    # Иначе можно потерять данные из исходного словаря
+    user_body = data.user_body.copy()
+    # Удаление параметра firstName из запроса
+    user_body.pop("firstName")
+    # Проверка полученного ответа
+    negative_assert_no_first_name(user_body)
+
+# Тест 11. Ошибка
+# Параметр fisrtName состоит из пустой строки
+def test_create_user_empty_first_name_get_error_response():
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body("")
+    # Проверка полученного ответа
+    negative_assert_no_first_name(user_body)
+
+def test_create_user_number_type_first_name_get_error_response():
+    user_body = get_user_body(12)
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 400
+    assert response.status_code == 400
---
+++ sender_stand_request.py
@@ -0,0 +1,33 @@
+# Импорт модуля requests для отправки HTTP-запросов
+import requests
+# Импорт конфигурационного файла, который содержит настройки URL
+import configuration
+# Импорт данных запроса из модуля data, в котором определены заголовки и тело запроса
+import data
+
+
+# Функция для получения данных из таблицы пользователей
+def get_users_table():
+    # Составление полного URL пути к данным таблицы пользователей
+    # путем конкатенации базового URL сервиса и конечной точки таблицы пользователей
+    # Возвращает объект ответа от сервера
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+# Определение функции для отправки POST-запроса на поиск наборов по продуктам
+def post_products_kits(products_ids):
+    # Отправка POST-запроса с использованием URL из конфигурации, данных о продуктах и заголовков
+    # Возвращается объект ответа, полученный от сервера
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,  # Тело запроса содержит ID продуктов в формате JSON
+                         headers=data.headers)  # Использование заголовков из файла data.py
+
